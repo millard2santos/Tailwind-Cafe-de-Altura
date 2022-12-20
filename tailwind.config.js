d31c9c613a5cbc10998ccd3fba43f6d27b2ba30d
millard2santos <millard2santos@gmail.com>
2022-12-20 17:53:45 +0100

    Tailwind-Installed

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    colors: {
+      grey: '#515051',
+      darkGrey: '#2B2A2B',
+      black: '#1F1815',
+      taupe: '#E3DED7',
+      green: '#2A5B45'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
