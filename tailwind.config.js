7362d7e055f6b5c37e7d54d5096aa32e29f93821
millard2santos <millard2santos@gmail.com>
2022-12-26 19:33:09 +0100

    Header-Listo

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,18 @@ module.exports = {
   content: ['./*.html'],
   theme: {
     colors: {
+      white: '#FFF',
       grey: '#515051',
       darkGrey: '#2B2A2B',
       black: '#1F1815',
       taupe: '#E3DED7',
       green: '#2A5B45'
     },
-    extend: {},
+    extend: {
+      boxShadow:{
+        normal : '0px 5px 5px rgba(0, 0, 0, 0.25)'
+      }
+    },
   },
   plugins: [],
 }
